3367ef010dc100980f07ce6e7f193e6d4da61484
0xSteak <dreintwink2004@gmail.com>
2024-10-23 23:45:57 +0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -262,5 +262,10 @@ ToggleButton.MouseButton1Click:Connect(function()
     end
 end)
 
+game.Players.LocalPlayer.Idled:Connect(function()
+    game:GetService("VirtualUser"):CaptureController()
+    game:GetService("VirtualUser"):ClickButton2(Vector2.new())
+end)
+
 shared.stop = function() stop = true end
 
